35dd259d4352d96f6a1a16b74bda36b7a8149f23
agentodessa <agentodessa2011@gmail.com>
2014-08-06 19:11:39 +0300

    Gallery Alpha

---
+++ Scripts/gallery.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+
+	$(".thumbs a").click(function () {
+
+		var largePath = $(this).attr("href");
+		var largeAlt = $(this).attr("title");
+
+		$(".largeimg").attr({ src: largePath, title: largeAlt });
+
+
+		$(".picture_counter").html(largeAlt);
+		return false;
+
+	});
+
+});
---
+++ obj/Release/Package/PackageTmp/Scripts/gallery.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function () {
+
+	//$("h2").append('<em></em>')
+
+	$(".thumbs a").click(function () {
+
+		var largePath = $(this).attr("href");
+		var largeAlt = $(this).attr("title");
+
+		$(".largeimg").attr({ src: largePath, alt: largeAlt });
+		 return false;
+	});
+
+});
