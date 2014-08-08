6adad287b6e22bfcd241ae15eb834971f553c3c4
agentodessa <agentodessa2011@gmail.com>
2014-08-08 18:07:04 +0300

    Code refactor

--- Scripts/gallery.js
+++ Scripts/gallery.js
@@ -1,17 +1,44 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
-	$(".thumbs a").click(function () {
+	$(".thumbs img").click(function () {
+		
+		var smallTitle =parseInt($(this).attr("title"));
 
-		var largePath = $(this).attr("href");
-		var largeAlt = $(this).attr("title");
-
-		$(".largeimg").attr({ src: largePath, title: largeAlt });
-
-
-		$(".picture_counter").html(largeAlt);
+		$(".gallery img").removeClass("opaque");
+		var newImg = $(this).parent().index();
+		$(".gallery img").eq(newImg).addClass("opaque");
+		$(".picture_counter").html(smallTitle);
 		return false;
-
 	});
 
+
+	var isAnimated;
+	var dx = 102;
+	$(".thumb_prev").click(function() {
+
+		var currentPos = parseInt($(".carousel").css('margin-left').replace(/[^-\d\.]/g, ''));
+		var offset;
+		(currentPos >= 0) ? offset = 0 : offset = currentPos + dx;
+		if (isAnimated) {
+			isAnimated = true;
+		}
+		$(".carousel").animate({ marginLeft: offset }, 500, function() {
+			isAnimated = false;
+		});
+});
+
+
+
+
+	$(".thumb_next").click(function () {
+		isAnimated = true;
+		var currentPos = parseInt($(".carousel").css('margin-left').replace(/[^-\d\.]/g, ''));
+		var maxWidth, offset;
+		maxWidth = ($(".thumbs img").length -1) * dx;
+		((-currentPos) >= maxWidth) ? offset = currentPos : offset = currentPos - dx;
+
+			$(".carousel").animate({ marginLeft: offset }, 500, function () {
+				isAnimated = false;
+			});
+	});
 });
--- obj/Release/Package/PackageTmp/Scripts/gallery.js
+++ obj/Release/Package/PackageTmp/Scripts/gallery.js
@@ -1,15 +1,47 @@
 ﻿
 $(document).ready(function () {
 
-	//$("h2").append('<em></em>')
+	$(".thumbs img").click(function () {
 
-	$(".thumbs a").click(function () {
+		var smallTitle =parseInt($(this).attr("title"));
 
-		var largePath = $(this).attr("href");
-		var largeAlt = $(this).attr("title");
+		$(".gallery img").removeClass("opaque");
+		var newImg = $(this).parent().index();
 
-		$(".largeimg").attr({ src: largePath, alt: largeAlt });
-		 return false;
+
+		$(".gallery img").eq(newImg).addClass("opaque");
+
+		$(".picture_counter").html(smallTitle);
+		return false;
 	});
+
+
+
+	var dx = 120;
+	$(".thumb_prev").click(function() {
+		var currentPos = parseInt($(".carousel").css('margin-left').replace(/[^-\d\.]/g, ''));
+		var offset;
+		(currentPos == 0) ? offset = 0 : offset = currentPos + dx;
+
+		//$(".carousel").css('margin-left', '' + offset + 'px');
+		$(".carousel").animate({ marginLeft: offset }, 800);
+	});
+
+
+
+
+	$(".thumb_next").click(function () {
+
+
+
+
+		var currentPos = parseInt($(".carousel").css('margin-left').replace(/[^-\d\.]/g, ''));
+		var maxWidth, offset;
+		maxWidth = $(".thumbs img").length * dx;
+		((-currentPos) >= maxWidth) ? offset = currentPos : offset = currentPos - dx;
+		//$(".carousel").css('margin-left', offset + 'px');
+		$(".carousel").animate({ marginLeft: offset }, 800);
+	});
+
 
 });
